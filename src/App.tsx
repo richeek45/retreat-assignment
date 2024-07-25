42a8a7caca09133fcab6c2c5fcd3b4b4ff789226
richeek45 <richeek45@gmail.com>
2024-07-25 20:22:07 +0530

    shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
+import { Button } from "@/components/ui/button"
 import './App.css'
 
 const getRetreats = async () => {
@@ -22,9 +23,19 @@ function App() {
   })
   console.log(data);
 
+  if (isPending) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+
   return (
     <div className="card">
-      <nav></nav>  
+      <nav className='bg-cyan-950	'>Wellness Retreat</nav> 
+      <Button>Working</Button> 
     </div>
   )
 }
