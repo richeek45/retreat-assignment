3a5b516238bf23b9ee601704dbddef0c02e8b15c
richeek45 <richeek45@gmail.com>
2024-07-25 20:13:43 +0530

    react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
+const getRetreats = async () => {
+  const response = await fetch(`https://669f704cb132e2c136fdd9a0.mockapi.io/api/v1/retreats/`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok')
+  }
+  return response.json()
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  // https://669f704cb132e2c136fdd9a0.mockapi.io/api/v1/retreats/
+  // https://669f704cb132e2c136fdd9a0.mockapi.io/api/v1/retreats?search=Wellness
+  // https://669f704cb132e2c136fdd9a0.mockapi.io/api/v1/retreats?location=Pune
+  // https://669f704cb132e2c136fdd9a0.mockapi.io/api/v1/retreats?page=1&limit=5
+
+  const { isPending, isError, data, error } = useQuery({ 
+    queryKey: ['todos'], 
+    queryFn: async () => await getRetreats() 
+  })
+  console.log(data);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+      <nav></nav>  
+    </div>
   )
 }
 
